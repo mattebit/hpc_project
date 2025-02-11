e6f5a1f0082ba6b87d02e5bd0737f2dd4f9ccd9c
Gatti <elia.gatti@hpc-head-n2.unitn.it>
2025-02-11 16:35:09 +0100

    changed to respect new implementations and graphs

--- parallel.sh
+++ parallel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#PBS -l select=1:ncpus=2:mem=32gb
+#PBS -l select=1:ncpus=64:mem=48gb
 
 # set max execution time
-#PBS -l walltime=0:05:00
+#PBS -l walltime=0:20:00
 
 # imposta la coda di esecuzione
 #PBS -q short_cpuQ
 module load mpich-3.2
-mpirun.actual -n 2 ~/hpc_project/parallel.o
+mpirun.actual -n 16 ~/hpc_project/src/main.o 50000 ~/hpc_project/graphs/graph_50000_dense.txt
