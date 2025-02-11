98a6fa4833e106aa7e2167378d0d61e1d482ad7d
Gatti <elia.gatti@hpc-head-n2.unitn.it>
2025-02-11 16:34:47 +0100

    added load of mpi

--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
-mpicc -std=c99 -O3 -fopenmp -march=native -mtune=native -ftree-vectorize -funroll-loops -flto -o src/main src/main.c
+module load mpich-3.2
+mpicc -std=c99 -O3 -fopenmp -march=native -mtune=native -ftree-vectorize -funroll-loops -flto -o src/main.o src/main.c
