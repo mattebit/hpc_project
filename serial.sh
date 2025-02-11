43fc10472144faab5498ac5d00aaa765cdc11d35
Gatti <elia.gatti@hpc-head-n2.unitn.it>
2025-02-11 16:35:40 +0100

    changed to respect new graphs location

--- serial.sh
+++ serial.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#PBS -l select=1:ncpus=1:mem=32gb
+#PBS -l select=1:ncpus=1:mem=16gb
 
 # set max execution time
-#PBS -l walltime=0:20:00
+#PBS -l walltime=0:05:00
 
 # imposta la coda di esecuzione
 #PBS -q short_cpuQ
-~/hpc_project/serial.o ~/hpc_project/large_graph.txt
+~/hpc_project/serial.o ~/hpc_project/graphs/graph_20000_dense.txt
